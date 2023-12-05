01307001bd8a7f2dd3a0fff570e1994a78135324
Julius Pallesen <julius@velosoph.de>
2023-12-05 21:27:04 +0100

    day 5 cleanup

--- 2023/Day5/d5p2.cpp
+++ 2023/Day5/d5p2.cpp
@@ -1,51 +1,51 @@
-#include <algorithm>
-#include <climits>
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <vector>
+#include <algorithm>    // For sort  
+#include <climits>      // For LLMAX
+#include <fstream>      // For file parsing
+#include <iostream>     // For printing
+#include <sstream>      // For string parsing
+#include <vector>       
 
 struct mapping {
-    long long source_start;
-    long long destination_start;
-    long long range;
+    uint64_t source_start;
+    uint64_t destination_start;
+    uint64_t range;
 };
 
-bool ranges_overlap(long long s1, long long e1, long long s2, long long e2)
+bool ranges_overlap(uint64_t s1, uint64_t e1, uint64_t s2, uint64_t e2)
 {
     return (s1 <= e2 && e1 >= s2) || (e1 >= s2 && s1 <= e2);
 }
 
-long long findMinFromRangeTree(const long long start, const long long end, const int map_index, std::vector<std::vector<mapping>>& maps)
+uint64_t findMinFromRangeTree(const uint64_t start, const uint64_t end, const int map_index, std::vector<std::vector<mapping>>& maps)
 {
-    long long min = LONG_MAX;
-    long long i = start;
+    uint64_t min = LLONG_MAX;
+    uint64_t i = start;
+    uint64_t value;
     if (map_index == maps.size() - 1) {
         min = start;
         for (mapping& m : maps[map_index]) {
             if (ranges_overlap(start, end, m.source_start, m.source_start + m.range)) {
-                long long finalMin = start + (m.destination_start - m.source_start);
-                min = min < finalMin ? min : finalMin;
+                value = start + (m.destination_start - m.source_start);
+                min = std::min(min, value);
             }
         }
     } else {
         for (mapping& m : maps[map_index]) {
             if (ranges_overlap(start, end, m.source_start, m.source_start + m.range)) {
-                long long value;
                 if (i < m.source_start) {
                     value = findMinFromRangeTree(i, m.source_start - 1, map_index + 1, maps);
-                    min = min < value ? min : value;
+                    min = std::min(min, value);
                 }
-                long long s = std::max(start, m.source_start) + (m.destination_start - m.source_start);
-                long long e = std::min(end, m.source_start + m.range) + (m.destination_start - m.source_start);
+                uint64_t s = std::max(start, m.source_start) + (m.destination_start - m.source_start);
+                uint64_t e = std::min(end, m.source_start + m.range) + (m.destination_start - m.source_start);
                 value = findMinFromRangeTree(s, e, map_index + 1, maps);
-                min = min < value ? min : value;
+                min = std::min(min, value);
                 i = m.source_start + m.range + 1;
             }
         }
         if (i < end) {
-            long long value = findMinFromRangeTree(i, end, map_index + 1, maps);
-            min = min < value ? min : value;
+            value = findMinFromRangeTree(i, end, map_index + 1, maps);
+            min = std::min(min, value);
         }
     }
     return min;
@@ -58,14 +58,16 @@ int main(int argc, char const* argv[])
     std::string line;
     std::string str;
     std::getline(inputFile, line);
-    std::vector<long long> seeds;
+    std::vector<uint64_t> seeds;
     std::stringstream ss(line);
-    std::vector<std::vector<mapping>> maps(7, std::vector<mapping>());
+    std::vector<std::vector<mapping>> maps;
+    uint8_t map_index = -1;
+
     ss >> str;
     while (ss >> str) {
         seeds.push_back(stoll(str));
     }
-    int map_index = -1;
+
     while (std::getline(inputFile, line)) {
         if (!line.empty()) {
             if (isdigit(line[0])) {
@@ -76,6 +78,7 @@ int main(int argc, char const* argv[])
                 ss >> m.range;
                 maps[map_index].push_back(m);
             } else {
+                maps.push_back(std::vector<mapping>());
                 map_index++;
             }
         }
@@ -85,10 +88,10 @@ int main(int argc, char const* argv[])
         std::sort(map.begin(), map.end(), [](const mapping& m, const mapping& n) { return m.source_start < n.source_start; });
     }
 
-    long long min_seed = LLONG_MAX;
+    uint64_t min_seed = LLONG_MAX;
     for (size_t i = 0; i < seeds.size(); i += 2) {
-        long long value = findMinFromRangeTree(seeds[i], seeds[i] + seeds[i + 1] - 1, 0, maps);
-        min_seed = min_seed < value ? min_seed : value;
+        uint64_t value = findMinFromRangeTree(seeds[i], seeds[i] + seeds[i + 1] - 1, 0, maps);
+        min_seed = std::min(min_seed, value);
     }
 
     inputFile.close();
