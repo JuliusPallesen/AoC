91d1b775ff00e4e7a2f7f7c95f91d40f16ba8824
Julius Pallesen <julius@velosoph.de>
2024-03-20 15:46:32 +0100

    day19 done (off by one error...)

--- 2023/Day19/d19p1.cpp
+++ 2023/Day19/d19p1.cpp
@@ -23,6 +23,24 @@ public:
         return x + m + a + s;
     }
 
+    unsigned int &operator[](const char c)
+    {
+        switch (c)
+        {
+        case 'x':
+            return x;
+        case 'm':
+            return m;
+        case 'a':
+            return a;
+        case 's':
+            return s;
+        default:
+            std::cerr << "not a variable.\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+
 private:
 };
 
@@ -30,13 +48,26 @@ class Rule
 {
 public:
     friend StepFactory;
-    Rule() {}
+    Rule() : operand(0), value(0), var(0) {}
     ~Rule() {}
-    std::shared_ptr<Step> apply(const MachinePart &m)
+    std::shared_ptr<Step> apply(MachinePart &m)
     {
-        return (condition(m)) ? target : nullptr;
+        if (operand)
+        {
+            if (operand == '<')
+            {
+                return m[var] < value ? target : nullptr;
+            }
+            else
+            {
+                return m[var] > value ? target : nullptr;
+            }
+        }
+        return target;
     };
-    std::function<bool(const MachinePart &)> condition;
+    char operand;
+    char var;
+    int value;
     std::shared_ptr<Step> target;
 };
 
@@ -44,7 +75,7 @@ class Step
 {
 public:
     friend StepFactory;
-    virtual bool isValid(const MachinePart &m) { return false; };
+    virtual bool isValid(MachinePart &m) { return false; };
     void addRule(std::shared_ptr<Rule> rule)
     {
         rules.push_back(rule);
@@ -59,7 +90,7 @@ class ConcreteStep : public Step
 {
 public:
     friend StepFactory;
-    bool isValid(const MachinePart &m) override
+    bool isValid(MachinePart &m) override
     {
         for (auto &&r : rules)
         {
@@ -77,7 +108,7 @@ class AcceptingStep : public Step
 {
 public:
     friend StepFactory;
-    bool isValid(const MachinePart &m) override
+    bool isValid(MachinePart &m) override
     {
         return true;
     }
@@ -90,7 +121,7 @@ class RejectingStep : public Step
 {
 public:
     friend StepFactory;
-    bool isValid(const MachinePart &m) override
+    bool isValid(MachinePart &m) override
     {
         return false;
     }
@@ -145,13 +176,12 @@ private:
         if (rulestr.find(":") == rulestr.npos)
         {
             target = rulestr;
-            r->condition = [](const MachinePart &m)
-            { return true; };
         }
         else
         {
-            std::function<bool(int, int)> comp = getCompFun(rulestr);
-            r->condition = getCondition(rulestr, comp);
+            r->var = rulestr[0];
+            r->operand = rulestr[rulestr.find_first_of("<>")];
+            r->value = stoi(rulestr.substr(rulestr.find_first_of("<>") + 1, rulestr.find(":") - 1));
             target = rulestr.substr(rulestr.find(':') + 1, rulestr.size());
         }
         tryToAddTarget(r, target);
@@ -179,52 +209,6 @@ private:
             step->addRule(makeRule(str));
         }
     }
-
-    std::function<bool(int, int)> getCompFun(const std::string &rulestr)
-    {
-        std::function<bool(int, int)> comp;
-        if (rulestr.find('<') != rulestr.npos)
-        {
-            comp = [](int a, int b)
-            { return a < b; };
-        }
-        else
-        {
-            comp = [](int a, int b)
-            { return a > b; };
-        }
-        return comp;
-    }
-
-    std::function<bool(const MachinePart &)> getCondition(std::string rulestr, std::function<bool(int, int)> comp)
-    {
-        std::string numstr = rulestr.substr(rulestr.find_first_of("<>") + 1, rulestr.find(":") - 1);
-        int value = stoi(numstr);
-        switch (rulestr.substr(0, rulestr.find_first_of("<>"))[0])
-        {
-        case 'x':
-            return [comp, value](const MachinePart &m)
-            { return comp(m.x, value); };
-            break;
-        case 'm':
-            return [comp, value](const MachinePart &m)
-            { return comp(m.m, value); };
-            break;
-        case 'a':
-            return [comp, value](const MachinePart &m)
-            { return comp(m.a, value); };
-            break;
-        case 's':
-            return [comp, value](const MachinePart &m)
-            { return comp(m.s, value); };
-            break;
-        default:
-            std::cerr << "unexpected char : " << rulestr.substr(0, rulestr.find("<>"))[0] << "\n";
-            exit(EXIT_FAILURE);
-            break;
-        }
-        return {};
-    }
 };
 
 int main(int argc, char const *argv[])
@@ -248,21 +232,7 @@ int main(int argc, char const *argv[])
         while (std::getline(ss, str, ','))
         {
             int val = stoi(str.substr(str.find('=') + 1, str.size()));
-            switch (str[0])
-            {
-            case 'x':
-                part.x = val;
-                break;
-            case 'm':
-                part.m = val;
-                break;
-            case 'a':
-                part.a = val;
-                break;
-            case 's':
-                part.s = val;
-                break;
-            }
+            part[str[0]] = val;
         }
         if (sf.getFirstStep()->isValid(part))
             ans += part.getSum();
--- 2023/Day19/d19p2.cpp
+++ 2023/Day19/d19p2.cpp
@@ -3,128 +3,204 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
-#include <string>
 #include <unordered_map>
-#include <functional>
 #include <memory>
+#include <optional>
 
-/*
-- Somehow inverse the tree.
-    - Rules point to their Step
-    - Steps point to all ingoing Rules
-
-- create a new part structure that can represent ranges of values
-
-- Start at A
-- iterate through all rules that point to A (have it as its target)
-- apply the rule to the range of the machine part
-- visit allincoming rules of the rules assosiated step
-
-- end at in. return (product of?) Ranged RangedMachinePart
-*/
-
-class StepFactory;
 class Step;
+class StepFactory;
 
-class RangedMachinePart
+struct MachinePartRange
 {
-public:
     struct range
     {
-        int from;
-        int to;
+        uint64_t from = 1;
+        uint64_t to = 4000;
     };
-    RangedMachinePart()
+    range x, m, a, s;
+
+    inline uint64_t getProd()
     {
-        ranges['x'] = {0, 4000};
-        ranges['m'] = {0, 4000};
-        ranges['a'] = {0, 4000};
-        ranges['s'] = {0, 4000};
+        return 1 * (x.to - x.from + 1) * (m.to - m.from + 1) * (a.to - a.from + 1) * (s.to - s.from + 1);
     }
-    std::unordered_map<char, range> ranges;
-    uint64_t getProd()
+
+    inline bool isValid()
     {
-        uint64_t prod = 1;
-        for (const auto &range : ranges)
+        return (x.to >= x.from) && (m.to >= m.from) && (a.to >= a.from) && (s.to >= s.from);
+    }
+
+    inline void print()
+    {
+        std::cout << "{"
+                  << "x: " << x.from << "-" << x.to << "\t"
+                  << "m: " << m.from << "-" << m.to << "\t"
+                  << "a: " << a.from << "-" << a.to << "\t"
+                  << "s: " << s.from << "-" << s.to << "}\n";
+    }
+
+    range &operator[](const char c)
+    {
+        switch (c)
         {
-            prod *= (range.second.to - range.second.from);
+        case 'x':
+            return x;
+        case 'm':
+            return m;
+        case 'a':
+            return a;
+        case 's':
+            return s;
+        default:
+            std::cerr << "not a variable.\n";
+            exit(EXIT_FAILURE);
         }
-        return prod;
-    };
+    }
 };
 
-class Rule
+struct MachinePart
 {
-public:
-    friend StepFactory;
-    Rule() {}
-    ~Rule() {}
-    int apply(RangedMachinePart m)
+    uint32_t x, m, a, s;
+
+    uint32_t getSum()
     {
-        return parent && restrictFunc(m) ? parent->apply(m) : 0;
+        return x + m + a + s;
+    }
+
+    uint32_t &operator[](const char c)
+    {
+        switch (c)
+        {
+        case 'x':
+            return x;
+        case 'm':
+            return m;
+        case 'a':
+            return a;
+        case 's':
+            return s;
+        default:
+            std::cerr << "not a variable.\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+};
+
+struct Rule
+{
+    char operand;
+    char var;
+    int value;
+    std::shared_ptr<Step> target;
+
+    std::shared_ptr<Step> apply(MachinePart &m)
+    {
+        if (!operand)
+            return target;
+
+        if (operand == '<')
+        {
+            return m[var] < value ? target : nullptr;
+        }
+        else
+        {
+            return m[var] > value ? target : nullptr;
+        }
     };
-    std::function<bool(RangedMachinePart &)> restrictFunc;
-    std::shared_ptr<Step> parent;
 };
 
 class Step
 {
 public:
     friend StepFactory;
-    virtual int apply(RangedMachinePart m) { return 0; };
-    std::vector<std::shared_ptr<Rule>> predecessors;
-    std::string name;
-    Step() = default;
-    virtual ~Step() = default;
 
-    void addPredecessor(std::shared_ptr<Rule> rule)
+    virtual bool isValidPart(MachinePart &m) { return false; }
+    virtual uint64_t getPossibilities(MachinePartRange m) { return 0; }
+
+protected:
+    Step() {}
+    virtual ~Step() {}
+    std::vector<std::shared_ptr<Rule>> rules;
+    std::string name;
+
+private:
+    void addRule(std::shared_ptr<Rule> rule)
     {
-        predecessors.push_back(rule);
+        rules.push_back(rule);
     }
 };
 
 class ConcreteStep : public Step
 {
 public:
-    friend StepFactory;
-    int apply(RangedMachinePart m) override
+    bool isValidPart(MachinePart &m) override
     {
-        for (auto &&pred : predecessors)
+        for (auto &&r : rules)
         {
-            RangedMachinePart m2 = m;
-            pred->apply(m2);
+            const std::shared_ptr<Step> next = r->apply(m);
+            if (next)
+                return next->isValidPart(m);
         }
+        return false;
+    }
 
-        if (name == "in")
+    uint64_t getPossibilities(MachinePartRange m) override
+    {
+        uint64_t possibilities = 0;
+        for (const auto &r : rules)
         {
-            return true;
+            const auto next = getNext(m, r);
+            if (next)
+            {
+                possibilities += r->target->getPossibilities(*next);
+            }
+            else
+            {
+                return possibilities + r->target->getPossibilities(m);
+            }
         }
+        return possibilities;
+    }
+
+private:
+    std::optional<MachinePartRange> getNext(MachinePartRange &m, const std::shared_ptr<Rule> &r)
+    {
+        if (!r->operand)
+            return {};
+        MachinePartRange next = m;
+        if (r->operand == '<')
+        {
+            m[r->var].from = r->value;      // Rest: x < 1000 | x = 500-4000 ==> x = 1000-4000
+            next[r->var].to = r->value - 1; // Next: x < 1000 | x = 500-4000 ==> x = 500-999
+        }
+        else
+        {
+            m[r->var].to = r->value;          // Rest: x > 1000 | x = 500-4000 ==> x = 500-1000
+            next[r->var].from = r->value + 1; // Next: x > 1000 | x = 500-4000 ==> x = 1001-4000
+        }
+        return next;
     }
 };
 
 class AcceptingStep : public Step
 {
 public:
-    friend StepFactory;
-    int apply(RangedMachinePart m) override
+    AcceptingStep() { name = "A"; }
+
+    bool isValidPart(MachinePart &m) override
     {
         return true;
     }
 
-    AcceptingStep() { name = "A"; }
-    ~AcceptingStep() {}
+    uint64_t getPossibilities(MachinePartRange m) override
+    {
+        return m.getProd();
+    }
 };
 
 class RejectingStep : public Step
 {
 public:
-    friend StepFactory;
-    int apply(RangedMachinePart m) override
-    {
-        return false;
-    }
     RejectingStep() { name = "R"; }
-    ~RejectingStep() {}
 };
 
 class StepFactory
@@ -135,7 +211,6 @@ public:
         steps["A"] = std::make_shared<AcceptingStep>();
         steps["R"] = std::make_shared<RejectingStep>();
     }
-    ~StepFactory() {}
 
     void makeStep(std::string stepstr)
     {
@@ -157,14 +232,12 @@ private:
 
     void notifyWaiters(std::shared_ptr<Step> step)
     {
-        if (waiting.count(step->name))
-        {
-            for (auto &&r : waiting[step->name])
-            {
-                //r->target = step;
-            }
-            waiting.erase(step->name);
-        }
+        if (waiting.count(step->name) == 0)
+            return;
+
+        for (auto &&r : waiting[step->name])
+            r->target = step;
+        waiting.erase(step->name);
     }
 
     std::shared_ptr<Rule> makeRule(std::string rulestr)
@@ -174,13 +247,12 @@ private:
         if (rulestr.find(":") == rulestr.npos)
         {
             target = rulestr;
-            r->restrictFunc = [](const RangedMachinePart &m)
-            { return true; };
         }
         else
         {
-            std::function<bool(int, int)> comp = getCompFun(rulestr);
-            r->restrictFunc = getrestrictFunc(rulestr, comp);
+            r->var = rulestr[0];
+            r->operand = rulestr[rulestr.find_first_of("<>")];
+            r->value = stoi(rulestr.substr(rulestr.find_first_of("<>") + 1, rulestr.find(":") - 1));
             target = rulestr.substr(rulestr.find(':') + 1, rulestr.size());
         }
         tryToAddTarget(r, target);
@@ -191,7 +263,7 @@ private:
     {
         if (steps.count(target))
         {
-            r->parent = steps[target];
+            r->target = steps[target];
         }
         else
         {
@@ -204,55 +276,7 @@ private:
         std::string str;
         std::stringstream ss(rulesstr);
         while (std::getline(ss, str, ','))
-        {
             step->addRule(makeRule(str));
-        }
-    }
-
-    std::function<bool(int, int)> getCompFun(const std::string &rulestr)
-    {
-        std::function<bool(int, int)> comp;
-        if (rulestr.find('<') != rulestr.npos)
-        {
-            comp = [](int a, int b)
-            { return a < b; };
-        }
-        else
-        {
-            comp = [](int a, int b)
-            { return a > b; };
-        }
-        return comp;
-    }
-
-    std::function<bool(const RangedMachinePart &)> getrestrictFunc(std::string rulestr, std::function<bool(int, int)> comp)
-    {
-        std::string numstr = rulestr.substr(rulestr.find_first_of("<>") + 1, rulestr.find(":") - 1);
-        int value = stoi(numstr);
-        switch (rulestr.substr(0, rulestr.find_first_of("<>"))[0])
-        {
-        case 'x':
-            return [comp, value](const RangedMachinePart &m)
-            { return comp(m.x, value); };
-            break;
-        case 'm':
-            return [comp, value](const RangedMachinePart &m)
-            { return comp(m.m, value); };
-            break;
-        case 'a':
-            return [comp, value](const RangedMachinePart &m)
-            { return comp(m.a, value); };
-            break;
-        case 's':
-            return [comp, value](const RangedMachinePart &m)
-            { return comp(m.s, value); };
-            break;
-        default:
-            std::cerr << "unexpected char : " << rulestr.substr(0, rulestr.find("<>"))[0] << "\n";
-            exit(EXIT_FAILURE);
-            break;
-        }
-        return {};
     }
 };
 
@@ -261,14 +285,34 @@ int main(int argc, char const *argv[])
     std::ifstream inputFile;
     inputFile.open(argv[1]);
     std::string line;
-    int ans = 0;
+    uint64_t ans = 0;
     StepFactory sf;
     while (std::getline(inputFile, line) && !line.empty())
     {
         sf.makeStep(line);
     }
-    RangedMachinePart rmp();
+
+    while (std::getline(inputFile, line))
+    {
+        line = line.substr(1, line.size() - 1);
+        std::stringstream ss(line);
+        std::string str;
+        MachinePart part;
+        while (std::getline(ss, str, ','))
+        {
+            int val = stoi(str.substr(str.find('=') + 1, str.size()));
+            part[str[0]] = val;
+        }
+        if (sf.getFirstStep()->isValidPart(part))
+            ans += part.getSum();
+    }
     inputFile.close();
-    std::cout << ans;
+    std::cout << "p1: " << ans << "\n";
+    MachinePartRange m;
+    ans = sf.getFirstStep()->getPossibilities(m);
+    std::cout << "p2: " << ans << "\n";
+    std::cout << "ex: 167409079868000\n";
+    // ans: 167409079868000
+    // now: 167289746868108
     return 0;
 }
