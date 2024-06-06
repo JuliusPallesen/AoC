80b40f32faee0f3725a40c04ea01e1c261188dc4
Julius Pallesen <julius@velosoph.de>
2024-06-06 16:57:31 +0200

    2022 Day02 part2 C

--- 2022/Day01/day1.c
+++ 2022/Day01/day1.c
@@ -1,39 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-/// Day One of the Advent of Code in C
-void main(int argc, char* argv[])
+#define ELFS_AMOUNT 3
+
+size_t get_min_index(int *max_arr, size_t size)
 {
-    char line[150];
-    int len = 0;
-    int sum = 0;
-    int num = 0;
-    int elfs = 0;
-    int max = 0;
-    if (argc > 0) {
-        char* file = argv[1];
-        FILE* fp = fopen(file, "r");
-        while (fgets(line, 150, fp) && !feof(fp)) {
-            sscanf(line, "%d/n", &num);
-            if (num != 0) {
-                sum += num;
-            } else {
-                elfs++;
-                printf("Elf %d has %d calories\n", elfs, sum);
-                if (sum > max) {
-                    max = sum;
-                }
-                sum = 0;
-            }
-            num = 0;
+    int min = INT_MAX;
+    size_t index = 0;
+    for (size_t i = 0; i < size; ++i)
+    {
+        if (min > max_arr[i])
+        {
+            index = i;
+            min = max_arr[i];
         }
-        if (sum > max) {
-            max = sum;
-        }
-        printf("Most calories: %d", max);
-        fclose(fp);
-        free(fp);
-    } else {
-        printf("Please provide a filepath");
     }
+    return index;
+}
+
+void main(int argc, char *argv[])
+{
+    if (argc <= 1)
+    {
+        printf("Please provide a filepath");
+        exit(1);
+    }
+    char line[16];
+    int max_arr[ELFS_AMOUNT] = {0, 0, 0};
+    size_t min_index = 0;
+
+    int sum = 0;
+
+    char *file = argv[1];
+    FILE *fp = fopen(file, "r");
+    do
+    {
+        int num = 0;
+        if (sscanf(line, "%d/n", &num) && num != 0)
+        {
+            sum += num;
+            continue;
+        }
+        if (max_arr[min_index] < sum)
+        {
+            max_arr[min_index] = sum;
+            min_index = get_min_index(max_arr, ELFS_AMOUNT);
+        }
+        sum = 0;
+    } while (fgets(line, 16, fp));
+
+    if (max_arr[min_index] < sum)
+    {
+        max_arr[min_index] = sum;
+        min_index = get_min_index(max_arr, ELFS_AMOUNT);
+    }
+
+    int max = 0;
+    sum = 0;
+    for (size_t i = 0; i < 3; i++)
+    {
+        if (max_arr[i] > max)
+            max = max_arr[i];
+        sum += max_arr[i];
+    }
+
+    printf("part1: %d\n", max);
+    printf("part2: %d\n", sum);
+    fclose(fp);
 }
