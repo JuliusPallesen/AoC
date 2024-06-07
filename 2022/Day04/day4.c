8b965b97b7e695ea935d7479bea7b691ef5b6f05
Julius Pallesen <julius@velosoph.de>
2024-06-07 10:46:56 +0200

    2022 Day4 Formatting

--- 2022/Day04/day4.c
+++ 2022/Day04/day4.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 
 /// Day Four of the Advent of Code in C
-void main(int argc, char* argv[])
+void main(int argc, char *argv[])
 {
     char line[150];
     int sum = 0;
     int s1, s2, e1, e2;
-    char* file = argv[1];
-    FILE* fp = fopen(file, "r");
-    while (fgets(line, 150, fp)) {
+    char *file = argv[1];
+    FILE *fp = fopen(file, "r");
+    while (fgets(line, 150, fp))
+    {
         sscanf(line, "%d-%d,%d-%d\n", &s1, &e1, &s2, &e2);
-        if (((s1 <= s2) && (s2 <= e1)) || ((s1 <= e2) && (e2 <= e1)) || ((s2 <= s1) && (s1 <= e2)) || ((s2 <= e1) && (e1 <= e2))) {
+        if (((s1 <= s2) && (s2 <= e1)) ||
+            ((s1 <= e2) && (e2 <= e1)) ||
+            ((s2 <= s1) && (s1 <= e2)) ||
+            ((s2 <= e1) && (e1 <= e2)))
+        {
             sum++;
         }
-        if (feof(fp)){
+        if (feof(fp))
+        {
             break;
         }
     }
