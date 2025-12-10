5050f7d8b7e025623e4df13ff421fdf48537bfa9
Julius Pallesen <julius@velosoph.de>
2025-12-10 16:50:52 +0100

    Cleanup

--- 2025/Day02/src/day2.cpp
+++ 2025/Day02/src/day2.cpp
@@ -1,144 +1,127 @@
-#include <cassert>
-#include <cmath>
 #include <concepts>
 #include <cstdint>
-#include <vector>
 #include <string>
 #include <fstream>
 #include <format>
 #include <algorithm>
 #include <iostream>
 #include <ranges>
-#include <concepts>
-#include <queue>
-#include <climits>
 
-// TODO noexcept ? template ?
-[[nodiscard]] constexpr auto ipow(std::uint64_t num, std::uint64_t exp) -> std::uint64_t
+template <std::integral T>
+[[nodiscard]] constexpr auto ipow(T num, T exp) noexcept -> T
 {
-    return std::ranges::fold_left(std::views::iota(0ull, exp), 1ull, [&](auto acc, auto)
-                                  { return acc * num; });
+    return std::ranges::fold_left(std::views::repeat(num, exp), T{1}, std::multiplies{});
 }
 
-[[nodiscard]] constexpr auto digit_count(std::uint64_t num, std::uint64_t base = 10) -> std::uint64_t
+template <std::integral T>
+[[nodiscard]] constexpr auto digit_count(T num, T base = T{10}) noexcept -> T
 {
-    auto iter = std::views::iota(0ull) | std::views::take_while([&](auto)
-                                                                { return (num /= base) > 0; });
+    auto iter = std::views::iota(0) | std::views::take_while([&](auto)
+                                                             { return (num /= base) > 0; });
     return 1 + std::ranges::distance(iter);
 }
 
-[[nodiscard]] constexpr auto to_digits(std::uint64_t n, std::uint64_t base = 10)
+template <std::integral T>
+[[nodiscard]] constexpr auto to_digits(T n, T base = T{10}) noexcept
 {
-    return std::views::iota(0ull, digit_count(n, base)) | std::views::transform([=](auto exp)
-                                                                                { return (n / ipow(base, exp)) % base; });
+    const auto digits = digit_count(n, base);
+    return std::views::iota(T{0}, digits) | std::views::transform([=](auto exp)
+                                                                  { return (n / ipow(base, exp)) % base; });
 }
 
+template <std::integral T>
 class IdRange
 {
 public:
-    constexpr IdRange(std::uint64_t from, std::uint64_t to) : from(from), to(to) {}
-    constexpr IdRange(std::string_view range)
+    constexpr IdRange(std::string_view range) noexcept
     {
-        const auto separator = range.find('-');
-        auto parse = [](std::string_view s)
+        const auto parse_int = [](std::string_view s)
         {
-            return std::ranges::fold_left(s, 0ull, [](auto acc, char c)
-                                          { return acc * 10 + (c - '0'); });
+            return std::ranges::fold_left(s, T{0}, [](auto acc, char c)
+                                          { return acc * T{10} + (c - '0'); });
         };
-        from = parse(range.substr(0, separator));
-        to = parse(range.substr(separator + 1));
-    }
-    ~IdRange() = default;
-    IdRange(const IdRange &other) = default;
-    IdRange &operator=(const IdRange &other) = default;
-    IdRange(IdRange &&other) noexcept = default;
-
-    [[nodiscard]] constexpr auto part1_sum() const -> std::uint64_t
-    {
-        return id_sum(valid_part1);
+        const auto separator = range.find('-');
+        from = parse_int(range.substr(0, separator));
+        to = parse_int(range.substr(separator + 1));
     }
 
-    [[nodiscard]] constexpr auto part2_sum() const -> std::uint64_t
-    {
-        return id_sum(valid_part2);
-    }
-
-    std::uint64_t from;
-    std::uint64_t to;
+    [[nodiscard]] constexpr auto part1_sum() const noexcept -> T { return id_sum(valid_part1); }
+    [[nodiscard]] constexpr auto part2_sum() const noexcept -> T { return id_sum(valid_part2); }
 
 private:
-    template <std::invocable<std::uint64_t> F>
-    [[nodiscard]] constexpr auto id_sum(F f) const -> std::uint64_t
+    template <std::invocable<T> F>
+    [[nodiscard]] constexpr auto id_sum(F f) const noexcept -> T
     {
         auto ids = std::views::iota(from, to + 1) | std::views::filter(f);
-        return std::ranges::fold_left(ids.begin(), ids.end(), std::uint64_t{}, [](auto acc, auto id)
-                                      { return acc + id; });
+        return std::ranges::fold_left(ids, T{0}, std::plus{});
     }
 
-    [[nodiscard]] constexpr static auto valid_part1(std::uint64_t num) -> bool
+    [[nodiscard]] constexpr static auto valid_part1(T num) noexcept -> bool
     {
         const auto digits = digit_count(num);
-
         if (digits % 2 == 1)
             return false;
-
-        // Create a "Zero sandwich" => 1001 =>  1234 * 1234 = 12341234 => valid ID!
-        const auto sandwich = ipow(10, digits / 2) + 1;
+        // https://www.youtube.com/watch?v=6JrFu0xj0bs
+        const auto sandwich = ipow(T{10}, digits / 2) + 1;
         return (num % sandwich) == 0;
     }
 
-    [[nodiscard]] constexpr static auto valid_part2(std::uint64_t num) -> bool
+    [[nodiscard]] constexpr static auto valid_part2(T num) noexcept -> bool
     {
         const auto middle = digit_count(num) / 2;
+        const auto digits = to_digits(num);
+        if (digits % 2 == 0)
+            return has_equal_chunks(digits, 1);
 
-        for (const auto &i : std::views::iota(1ull, middle + 1))
-            if (has_equal_chunks(to_digits(num), i))
-                return true;
-
-        return false;
+        return std::ranges::any_of(std::views::iota(T{1}, middle + 1), [num, &digits](const auto i)
+                                   { return has_equal_chunks(digits, i); });
     }
 
-    [[nodiscard]] constexpr static auto has_equal_chunks(auto num, size_t window_size) -> bool
+    [[nodiscard]] constexpr static auto has_equal_chunks(const auto num, T window_size) noexcept -> bool
     {
         auto chunks = num | std::views::chunk(window_size);
-        auto first_chunk = *chunks.begin();
-
+        const auto first_chunk = *chunks.begin();
         return std::ranges::all_of(chunks, [first_chunk](auto chunk)
                                    { return std::ranges::equal(chunk, first_chunk); });
     }
+
+    T from;
+    T to;
 };
 
-[[nodiscard]] constexpr auto part1(std::string_view input) -> std::uint64_t
+template <std::integral T, typename F>
+[[nodiscard]] constexpr auto solve_part(auto ranges, F f) noexcept -> T
 {
-    auto strings = std::views::split(input, ',');
-    return std::ranges::fold_left(strings, 0ull, [](const auto acc, auto str)
-                                  {
-            const auto view = std::string_view{str.begin(), str.end()};
-            const IdRange range{view}; 
-            return acc + range.part1_sum(); });
+    auto sums = ranges | std::views::transform([&](auto str)
+                                               { return f(IdRange<T>{std::string_view{str.begin(), str.end()}}); });
+    return std::ranges::fold_left(sums, T{0}, std::plus{});
 }
 
-[[nodiscard]] constexpr auto part2(std::string_view input) -> std::uint64_t
+template <std::integral T>
+[[nodiscard]] constexpr auto part1(std::string_view input) noexcept -> T
 {
-    auto strings = std::views::split(input, ',');
-    return std::ranges::fold_left(strings, 0ull, [](auto acc, auto str)
-                                  {
-            const auto view = std::string_view{str.begin(), str.end()};
-            const IdRange range{view}; 
-            return acc + range.part2_sum(); });
+    return solve_part<T>(std::views::split(input, ','), [](const auto &r)
+                         { return r.part1_sum(); });
+}
+
+template <std::integral T>
+[[nodiscard]] constexpr auto part2(std::string_view input) noexcept -> T
+{
+    return solve_part<T>(std::views::split(input, ','), [](const auto &r)
+                         { return r.part2_sum(); });
 }
 
 constexpr const static std::string_view example{"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"};
-static_assert(part1(example) == 1227775554);
-static_assert(part2(example) == 4174379265);
+static_assert(part1<std::uint64_t>(example) == std::uint64_t{1227775554});
+static_assert(part2<std::uint64_t>(example) == std::uint64_t{4174379265});
 
 auto solve(int argc, char const *argv[]) -> void
 {
     const std::string input_path{argc <= 1 ? "../input.txt" : argv[1]};
     std::ifstream input_file(input_path);
     std::string content((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
-    std::cout << std::format("part 1: {}\n", part1(content));
-    std::cout << std::format("part 2: {}\n", part2(content));
+    std::cout << std::format("part 1: {}\n", part1<std::uint64_t>(content));
+    std::cout << std::format("part 2: {}\n", part2<std::uint64_t>(content));
 }
 
 int main(int argc, char const *argv[])
@@ -147,7 +130,7 @@ int main(int argc, char const *argv[])
     {
         solve(argc, argv);
     }
-    catch (const std::runtime_error &e)
+    catch (const std::exception &e)
     {
         std::cerr << std::format("{}\n", e.what());
     }
